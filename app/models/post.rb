97ebed67a640778a2049adefca6cec55b13b110b
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-10 17:29:12 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w("Fiction" "Non-Fiction")}
+ 
+
+  CLICKBAIT = [
+    "Won't Believe", 
+    "Secret", 
+    "Top", 
+    "Guess"
+  ]
+
+  def custom
+    unless self.title.include?(CLICKBAIT)
+      errors.add(:title, "Nope")
+    end
+  end
 end
